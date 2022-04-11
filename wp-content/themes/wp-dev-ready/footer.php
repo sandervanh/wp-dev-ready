6dfc335e456df4fc01ea89376adfe39d0f2f2e27
Sander van Hooff <mail@sandervanhooff.com>
2022-04-11 15:56:56 +0200

    Added custom post types and taxonomies

--- wp-content/themes/wp-dev-ready/footer.php
+++ wp-content/themes/wp-dev-ready/footer.php
@@ -1,2 +1,20 @@
 </div>
+<footer>
+
+</footer>
+<script>
+    (function() {
+        if (!window.console) {
+            return;
+        }
+        var i = 0;
+        if (!i) {
+            setTimeout(function() {
+                console.log('%c👋 Hey there!', 'font: 2em sans-serif; line-height: 1.7;');
+                console.log('%cWhat brought you here? Are you just curious about the tech this site uses or do you have some issues?\nEither way you can contact us at info@vuewer.com or go to our website: https://vuewer.com for any issues or questions.', 'font: 1.25 sans-serif; line-height: 1.7;');
+            }, 1);
+            i = 1;
+        }
+    })();
+</script>
 </body>
